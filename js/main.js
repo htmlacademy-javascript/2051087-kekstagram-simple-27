ca05b2bb693257cf195eaefc8eb406aace792497
Nikita <tereshkonikita@mail.ru>
2022-10-03 21:25:25 +0300

    Вносит правки в тела функций

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-// функция, определяющая случайно генерированное число в диапазоне [min, max]
+// функция, определяющая случайно генерированное число в диапазоне [min, max)
 // const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min); - один из вариантов стрелочного объявения
+// isNaN() - функция, определяющая не число. Отдает булевый признак
 
-function getRandomNumber(min, max) {
-  if (min < 0 || max < 0 || isNaN(min) || isNaN(max) || typeof min === 'string' || typeof max === 'string') {
-    return 'NaN';
-  } else if (min >= max) {
-    return `Меняю аргументы местами и в итоге отдаю ${Math.round(Math.random() * (min - max + 1) + max)}`;
+function getRandomNumber(min = 0, max = 10) {
+  if (min < 0 || max < 0 || !isFinite(min) || !isFinite(max) || typeof min === 'string' || typeof max === 'string') {
+    throw new Error('NaN');
   }
-  return Math.round(Math.random() * (max - min + 1) + min);
+  if (min >= max) {
+    return `Меняю аргументы местами и в итоге отдаю ${Math.floor(Math.random() * (min - max + 1) + max)}`;
+  }
+  return `Рандомное значение - ${Math.floor(Math.random() * (max - min + 1) + min)}`;
 }
 getRandomNumber(5, 10);
 
 // функция, принимающая строку и максимальное возможное значение строки и возвращающая булевое значение
 
-function isAllowedLength (string, maxValue) {
-  const currentValue = string.length;
-  if (currentValue > maxValue) {
-    return false;
-  }
-  return true;
+function isAllowedLength(string, maxValue) {
+  return String(string).length <= maxValue;
 }
 isAllowedLength('keks', 9);
