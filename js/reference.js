9e90083dfe0c9f572082c3f5d38418f608d48d65
Nikita <tereshkonikita@mail.ru>
2022-10-20 23:43:10 +0300

    Программирует работу мессенджера

--- js/reference.js
+++ js/reference.js
@@ -1,6 +1,6 @@
 // файл, в котором хранятся более сложные решения задач
 
-//В файле main.js на основе написанных по заданию ранее вспомогательных функций напишите необходимые функции для создания массива из 25 сгенерированных объектов.
+// 1. В файле main.js на основе написанных по заданию ранее вспомогательных функций напишите необходимые функции для создания массива из 25 сгенерированных объектов.
 //Каждый объект массива — описание фотографии, опубликованной пользователем.
 
 /*
@@ -91,4 +91,79 @@ const arrayOfObjects = () => Array.from({length: SIMILAR_DESCRIPTION_COUNT}, (_,
 
 arrayOfObjects();
 
+*/
+
+// 2. Нужно запрограммировать мессенджер. Отправка сообщения, удаление сообщения.
+
+// Разметка
+
+/*
+<!DOCTYPE html>
+<html lang="ru">
+  <head>
+    <meta charset="utf-8">
+    <title>Чат</title>
+    <link href="style.css" rel="stylesheet">
+  </head>
+  <body>
+    <section class="chat">
+      <h1 class="visually-hidden">Чат</h1>
+
+      <div class="chat-content">
+
+      </div>
+
+      <form class="chat-form" action="https://echo.htmlacademy.ru/courses" method="post">
+        <input class="chat-form-input" type="text" aria-label="Ваше сообщение" placeholder="Напишите что-нибудь" required>
+        <button class="chat-form-button" type="submit">Отправить</button>
+      </form>
+    </section>
+
+    <template id="message-template">
+      <div class="chat-message" tabindex="0">
+        <span class="chat-message-name">Неопознанный енот</span>
+        <p class="chat-message-text"></p>
+        <button class="chat-message-button" type="button">Удалить</button>
+      </div>
+    </template>
+
+    <script src="script.js"></script>
+  </body>
+</html>
+*/
+
+// Код
+
+/*
+var form = document.querySelector('.chat-form');
+var inputText = form.querySelector('.chat-form-input');
+var chat = document.querySelector('.chat-content');
+var message = chat.children;
+var temp = document.querySelector('#message-template').content;
+var wrapper = temp.querySelector('.chat-message');
+var parag = temp.querySelector('.chat-message-text');
+
+
+var deleteMessage = function (mess) {
+  var deleButton = mess.querySelector('.chat-message-button');
+  deleButton.addEventListener('click', function () {
+    mess.remove();
+  });
+};
+
+for (var i = 0; i < message.length; i++) {
+  deleteMessage(message[i]);
+}
+
+form.addEventListener('submit', function(evt) {
+  evt.preventDefault();
+  var messText = inputText.value;
+  var wrapClone = wrapper.cloneNode(true);
+  var description = wrapClone.querySelector('.chat-message-text');
+  description.textContent = messText;
+  chat.appendChild(wrapClone);
+  inputText.value = '';
+
+  deleteMessage(wrapClone);
+  })
 */
