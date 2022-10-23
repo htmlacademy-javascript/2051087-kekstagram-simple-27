1b6935c4e4a42affc754ff4ca12818851d7fcd6b
Nikita <tereshkonikita@mail.ru>
2022-10-23 16:20:08 +0300

    Редактирует файл-точку входа

--- js/main.js
+++ js/main.js
@@ -1,6 +1,2 @@
-import { getDescriptionArray } from './generate-mock-data.js';
-import './print-photo.js';
-
 // window.getDescriptionArray() - для вызова из консоли
-
-getDescriptionArray();
+import './print-photo.js';
