70a022a1fd0cb5fe22def9af7eff7302b1acfb3d
Nikita <tereshkonikita@mail.ru>
2022-10-29 23:10:57 +0300

    Добавляет базовое поведение формы

---
+++ js/image-upload-form.js
@@ -0,0 +1,86 @@
+import { isEscapeKey } from './util.js';
+
+const imageForm = document.querySelector('.img-upload__form');
+const imgUploadInput = imageForm.querySelector('#upload-file');
+const imageEditingForm = imageForm.querySelector('.img-upload__overlay');
+const closeEditorFormButton = imageForm.querySelector('#upload-cancel');
+const decreaseButton = imageEditingForm.querySelector('.scale__control.scale__control--smaller');
+const increaseButton = imageEditingForm.querySelector('.scale__control.scale__control--bigger');
+const scaleField = imageEditingForm.querySelector('.scale__control.scale__control--value');
+const imageScale = imageEditingForm.querySelector('[data-preview-image="image"]');
+const pictureEffectButtons = imageEditingForm.querySelectorAll('.effects__radio');
+
+function onPopupEscKeydown(evt) {
+  if (isEscapeKey(evt)) {
+    evt.preventDefault();
+    closeImageEditingForm();
+  }
+}
+
+function openImageEditingForm() {
+  imageEditingForm.classList.remove('hidden');
+  imageScale.className = '';
+  document.querySelector('body').classList.add('modal-open');
+
+  document.addEventListener('keydown', onPopupEscKeydown);
+}
+
+function closeImageEditingForm() {
+  imageEditingForm.classList.add('hidden');
+  imageScale.className = '';
+  document.querySelector('body').classList.remove('modal-open');
+
+  document.removeEventListener('keydown', onPopupEscKeydown);
+}
+
+// imgUploadInput.addEventListener('click', () => {
+//   openImageEditingForm();
+// });
+
+imgUploadInput.addEventListener('change', () => {
+  openImageEditingForm();
+});
+
+closeEditorFormButton.addEventListener('click', () => {
+  closeImageEditingForm();
+  imgUploadInput.value = '';
+});
+
+closeEditorFormButton.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    closeImageEditingForm();
+    imgUploadInput.value = '';
+  }
+});
+
+decreaseButton.addEventListener('click', () => {
+  const currentScale = parseInt(scaleField.value);
+  const minValue = parseInt(scaleField.min);
+  const stepValue = parseInt(scaleField.step);
+  if (currentScale > minValue) {
+    const newScale = currentScale - stepValue;
+    scaleField.value = `${newScale}%`;
+    imageScale.style.transform = `scale(${newScale / 100})`;
+  }
+});
+
+increaseButton.addEventListener('click', () => {
+  const currentScale = parseInt(scaleField.value);
+  const maxValue = parseInt(scaleField.max);
+  const stepValue = parseInt(scaleField.step);
+  if (currentScale < maxValue) {
+    const newScale = currentScale + stepValue;
+    scaleField.value = `${newScale}%`;
+    imageScale.style.transform = `scale(${newScale / 100})`;
+  }
+});
+
+pictureEffectButtons.forEach((radio) => {
+  radio.addEventListener('change', () => {
+    const elementId = radio.id;
+    const classEffect = imageEditingForm.querySelector(`[for=${ elementId }]`).querySelector('.effects__preview');
+    imageScale.className = '';
+    imageScale.classList.add(`${classEffect.classList[1]}`);
+  });
+});
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 // window.getDescriptionArray() - для вызова из консоли
 import { printPhotos } from './print-photo.js';
+import './image-upload-form.js';
 printPhotos();
